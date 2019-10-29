694fa131694f88821130ffb45ae912b1bcd4b967
aurrrum <44933360+aurrrum@users.noreply.github.com>
2019-10-29 12:39:16 +0300

    Conditions

--- js/conditions.js
+++ js/conditions.js
@@ -1,4 +1,4 @@
-let a = 2*4;
+let a = 2 * 4;
 
 if (a == 8) {
     console.log("8");
@@ -6,3 +6,30 @@ if (a == 8) {
     console.log(a);
 }
 
+a = 40;
+
+if (a < 50) {
+    console.log("lt " + a);
+} else if (a > 50) {
+    console.log("gt" + a)
+}
+else {
+    console.log(a);
+}
+
+(a == 50) ? console.log("a = 50"): console.log("a != 50, a = " + a);
+
+switch (a) {
+    case a < 50:
+        console.log("lt " + a);
+        break;
+    case a > 50:
+        console.log("lt " + a);
+        break;
+    case 50:
+        console.log("a = " + a);
+        break;
+    default:
+        console.log("a = " + a);
+        break;
+}
